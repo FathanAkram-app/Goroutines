83078c1fcb64ac3a7bf6dc81ea43fdc97619218c
Fathan Akram <fathanakram@fathanakram.local>
2023-02-23 10:00:57 +0700

    init

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func say(s string, repeat int) {
+
+	for ; 0 < repeat; repeat-- {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 100)
+	}
+	wg.Done()
+
+}
+
+func main() {
+	wg.Add(1)
+	go say("Halo", 3)
+	wg.Add(1)
+	go say("Semua", 3)
+	wg.Wait()
+
+	wg.Add(1)
+	go say("Apa kabar?", 1)
+	wg.Wait()
+
+}
